c004a4c1103072e4d4957daf502037d504309f32
levko <levkosvd@gmail.com>
2020-01-28 14:18:51 +0200

    (:

--- src/main/java/mate/academy/internetshop/controller/LogoutController.java
+++ src/main/java/mate/academy/internetshop/controller/LogoutController.java
@@ -17,7 +17,6 @@ public class LogoutController extends HttpServlet {
             throws ServletException, IOException {
         bucketService.deleteById(bucketService.getByUser((Long) req.getSession().getAttribute("userId")).getId());
         req.getSession(false).invalidate();
-
         for (Cookie cookie : req.getCookies()) {
             if (cookie.getName().equals("MATE")) {
                 cookie.setMaxAge(0);
